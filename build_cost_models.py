b0842eda0aada009391bc0a4efdd713068140d80
Orthel Toralen <orthel.toralen@gmail.com>
2025-10-27 05:59:43 -0400

    expanded build_cost model to include regions

--- build_cost_models.py
+++ build_cost_models.py
@@ -1,26 +1,31 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, select, text
 from sqlalchemy.orm import declarative_base
+from config import DatabaseConfig
 
-build_cost_db = "build_cost.db"
-build_cost_url = f"sqlite:///{build_cost_db}"
+# build_cost_db = "build_cost.db"
+# build_cost_url = f"sqlite:///{build_cost_db}"
+
+build_cost_db = DatabaseConfig("build_cost")
+build_cost_url = build_cost_db.url
 
 Base = declarative_base()
 
 class Structure(Base):
     __tablename__ = "structures"
-    system = Column(String)
-    structure = Column(String)
-    system_id = Column(Integer)
+    system = Column(String, nullable=True)
+    structure = Column(String, nullable=True)
+    system_id = Column(Integer, nullable=True)
     structure_id = Column(Integer, primary_key=True)
-    rig_1 = Column(String)
-    rig_2 = Column(String)
-    rig_3 = Column(String)
-    structure_type = Column(String)
-    structure_type_id = Column(Integer)
-    tax = Column(Float)
-
+    rig_1 = Column(String, nullable=True)
+    rig_2 = Column(String, nullable=True)
+    rig_3 = Column(String, nullable=True)
+    structure_type = Column(String, nullable=True)
+    structure_type_id = Column(Integer, nullable=True)
+    tax = Column(Float, nullable=True)
+    region = Column(String, nullable=True)
+    region_id = Column(Integer, nullable=True)
     def __repr__(self):
-        return f"<Structure(system={self.system}, structure={self.structure}, system_id={self.system_id}, structure_id={self.structure_id}, rig_1={self.rig_1}, rig_2={self.rig_2}, rig_3={self.rig_3}, structure_type={self.structure_type}, structure_type_id={self.structure_type_id}, tax={self.tax})>"
+        return f"<Structure(system={self.system}, structure={self.structure}, system_id={self.system_id}, structure_id={self.structure_id}, rig_1={self.rig_1}, rig_2={self.rig_2}, rig_3={self.rig_3}, structure_type={self.structure_type}, structure_type_id={self.structure_type_id}, tax={self.tax}, region={self.region}, region_id={self.region_id})>"
 
 class IndustryIndex(Base):
     __tablename__ = "industry_index"
